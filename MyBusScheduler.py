b16857c900031607806904f42b241cafb54c47ef
Nick Heisler <nheisler@iastate.edu>
2019-02-14 18:09:32 -0600

    starting on making the app work

--- MyBusScheduler.py
+++ MyBusScheduler.py
@@ -12,7 +12,7 @@ bnorth = '862'
 #stops
 towers = '8860'
 
-print(next.get_predictions_for_stop(local, towers))
+query = next.get_predictions_for_stop(local, towers)
 
-#so I know how to pull the data into an xml from nextbus. Now I just need to parse the xml for relevent data
-#then print the data wherever it needs to go
+#so query will store the results of the get_predictions call
+#and I just need to parse that for relevant info
