c09aa4d711abe4150e4d4a6a1a801558135e7a72
Nick Heisler <nheisler@iastate.edu>
2019-02-19 13:35:22 -0600

    fixed the setup.py file

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
-from setuptools import setup
+from distutils.core import setup
 
-def get_readme():
-    return open('README.md').read()
-
-setup(name='PyScanner',
-      version='1.0',
-      packages=['scanner'],
-      description='Java-like input scanner.',
-      long_description=get_readme(),
-      author='Kanat Bekt',
-      author_email='bekt17@gmail.com',
-      url='https://github.com/Bekt/PyScanner'
+setup(
+    name        = 'python-nextbus',
+    description = 'A Python library for connecting to the NextBus public API',
+    version     = '0.1',
+    py_modules  = ['nextbus'],
+    url         = 'https://github.com/apparentlymart/python-nextbus',
 )
