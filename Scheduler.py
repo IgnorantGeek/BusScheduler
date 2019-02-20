5823c761630d0a18d7444b7ae60fc03ff47bc165
Nick Heisler <nheisler@iastate.edu>
2019-02-20 16:12:28 -0600

    preparing for v1.0 update

--- Scheduler.py
+++ Scheduler.py
@@ -4,54 +4,55 @@ import Tkinter as tk
 #ISU agency
 cyride = 'cyride'
 
-#routes I use
+#routes for testing
 gn = '952'
 gs = '951'
 bn = '862'
 bs = '861'
 
-#stops by me
+#stops for testing
 tturn = '8860'
 storm = '8870'
 
-def get_stops_for_route(agency, route):
-        stops = nextbus.get_route_config(agency, route)
+def print_stops_for_route(agency, route_id):
+        stops = nextbus.get_route_config(agency, route_id)
         for stop in stops.stops:
                 print "Stop at {} has stop_id {}".format(stop.title, stop.tag)
 
-def get_route_tags(agency):
+def print_route_tags(agency):
         routes = nextbus.get_all_routes_for_agency(agency)
         for route in routes:
                 print "Route {} has route_tag {}".format(route.title, route.tag)
 
-def prediction_query(agency, stop_id):
-        query = nextbus.get_predictions_for_stop(agency, stop_id)
-        if not query.predictions:
-                return -1
-        else:
-                return query.predictions[0].minutes
-
 # Can probably integrate the above method here. Pass in the routes and stops you want predictions for and it will
 # pull up a window with the readout
-def send_to_window():
+def prediction_to_window(agency, stop_id, *route_id):
         window = tk.Tk()
-        window.title('Next Arrivals at Storm St./Towers Turnaround')
-        while window:
-                goldtime = prediction_query(cyride, tturn)
-                browntime = prediction_query(cyride, storm)
-                tk.Label(window, text="Next Gold North: ", font=("Arial Bold", 30)).grid(row=0,column=0)
-                tk.Label(window, text=goldtime, font=("Arial Bold", 30)).grid(row=0,column=1)
-                tk.Label(window, text="  minutes", font=("Arial Bold", 30)).grid(row=0,column=2)
-                tk.Label(window, text="Next Brown North: ", font=("Arial Bold", 30)).grid(row=1,column=0)
-                tk.Label(window, text=browntime, font=("Arial Bold", 30)).grid(row=1,column=1)
-                tk.Label(window, text="  minutes", font=("Arial Bold", 30)).grid(row=1,column=2)
-                window.update()
+        query = nextbus.get_predictions_for_stop(agency, stop_id) #scan this query for the specified route_id
+        #how can we pass in multiple routes? We don't want endless parameters so how can we allow a user to 
+        #used send_to_window(agency, stop_id, route_id, route_id, route_id, etc.)
+        for route in route_id:
+                for prediction in query.predictions:
+                        #search for the specified route id in predictions, if you get a match send the first prediction
+                        #you find to the window method below. So we will probably need to run that in here
+                        route_title = prediction.direction.route.title
+                        arrival_time = prediction.minutes
+                        if prediction.direction.route.tag == route:
+                                tk.Label(window, text= route_title + ': ', font=("Arial Bold", 30)).grid(row=0,column=0)
+                                tk.Label(window, text=arrival_time, font=("Arial Bold", 30)).grid(row=0,column=1)
+                                tk.Label(window, text=" minutes", font=("Arial Bold", 30)).grid(row=0,column=2)
+                                window.update()
+                                break
+        window.title('Next Arrivals')
         window.mainloop()
 
 
+#main
+#prediction_to_window(cyride,tturn, gn, bn)
+
 #issues: 
 #        the window flashes when it is running, not a big  but kind of annoying
-#        there is a python error when closing the window because the loop tries to run again after it was destroyed, error in tk.Label()
-#        sometimes the display gets messed up with what number to display **hasnt happened in a while, untested**
+#        fixed the error when closing but now the window does not continuously update. Need a loop at the end of prediction_to_window() method
+#        sometimes the display gets messed up with what number to display
 #        need to fix the labels so this can be more of a multipurpose app
 #        handle printing -1 value for no prediction (fix with update v1.0)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name        = 'Bus Scheduler',
     description = 'A python library for getting the predictions for a bus stop using NextBus',
-    version     = '0.2',
+    version     = '0.6',
     py_modules  = ['nextbus'],
     url         = 'https://github.com/IgnorantGeek/BusScheduler.git',
 )
