60a4d854c282f88a35d09dbfd687331bff2a3f09
Nick Heisler <nheisler@iastate.edu>
2019-02-20 01:11:17 -0600

    updated some notes, getting ready for v1.0 update

--- Scheduler.py
+++ Scheduler.py
@@ -1,16 +1,16 @@
 import nextbus
 import Tkinter as tk
 
-#agency
+#ISU agency
 cyride = 'cyride'
 
-#routes
+#routes I use
 gn = '952'
 gs = '951'
 bn = '862'
 bs = '861'
 
-#stops
+#stops by me
 tturn = '8860'
 storm = '8870'
 
@@ -31,10 +31,6 @@ def prediction_query(agency, stop_id):
         else:
                 return query.predictions[0].minutes
 
-# I want to give this method a list of the  routes to print, then print them
-# So I will need to pass in a list of predictions from prediction_query and print the title and arrival time
-# So can I just pass in the prediction? IE have prediction_query return the whole prediction for specified routes
-# then send it to the window. Probably need to tweak the logic but that seems like a decent idea.
 # Can probably integrate the above method here. Pass in the routes and stops you want predictions for and it will
 # pull up a window with the readout
 def send_to_window():
@@ -54,8 +50,8 @@ def send_to_window():
 
 
 #issues: 
-#        the window flashes when it is running, not a big deal
-#        there is an error when closing the window because the loop tries to run again after it was destroyed.
-#        sometimes the display gets messed up with what number to display
+#        the window flashes when it is running, not a big  but kind of annoying
+#        there is a python error when closing the window because the loop tries to run again after it was destroyed, error in tk.Label()
+#        sometimes the display gets messed up with what number to display **hasnt happened in a while, untested**
 #        need to fix the labels so this can be more of a multipurpose app
-#        handle printing -1 value for no prediction
+#        handle printing -1 value for no prediction (fix with update v1.0)
