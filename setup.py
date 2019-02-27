5b5615dbfff4191b0a98f890f6744b51e9c36505
Nick Heisler <nheisler@iastate.edu>
2019-02-27 17:56:33 -0600

    updated setup

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name        = 'Bus Scheduler',
     description = 'A python library for getting the predictions for a bus stop using NextBus',
     version     = '0.6',
-    py_modules  = ['nextbus'],
     url         = 'https://github.com/IgnorantGeek/BusScheduler.git',
 )
 
