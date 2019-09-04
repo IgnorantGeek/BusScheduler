935a6110f140a95a4b4491065aa0b0f5949a99e3
Nick Heisler <nheisler@iastate.edu>
2019-09-04 14:11:15 -0500

    removed java files

--- Java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test
-{
-    public static void main(String[] args)
-    {
-        NextBus n = new NextBus();
-        n.add_agency_by_tag("cyide");
-        n.agencies.get(0).print_routes();
-    }
-}
